eb8b142c3f5e508de96537d3be4ef47910eeb90b
Nihalbacker <nihalbackerak@gmail.com>
2023-10-28 15:43:34 +0530

    commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  
+  runApp(MaterialApp(home: uidesign(),);
+  
+}
+class Uidesign extends StatefulWidget {
+  const uidesign({super.key});
+
+  @override
+  State<uidesign> createState() => _uidesignState();
+}
+
+class _uidesignState extends State<uidesign> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+  
+  
+}
